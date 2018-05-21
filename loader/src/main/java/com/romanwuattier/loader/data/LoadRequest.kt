fe9d9c13263579da001967eec5782e136daa70d1
Roman Wuattier <roman.wuattier@gmail.com>
2018-05-22 00:01:11 +0200

    Update `Local` and `Remote` `Request` kdoc

--- loader/src/main/java/com/romanwuattier/loader/data/LoadRequest.kt
+++ loader/src/main/java/com/romanwuattier/loader/data/LoadRequest.kt
@@ -9,10 +9,17 @@ internal interface Request
 /**
  * A [RemoteRequest] consists in:
  * <ul>
- *     <li>Loading data from a specific [url]</li>
+ *     <li>Loading data from a specific remote [url]</li>
  *     <li>Deserializing data in a [ConcurrentHashMap] according to a [ConverterType]</li>
  * </ul>
  */
 internal data class RemoteRequest(val url: String, val cacheDir: File, val converterType: ConverterType) : Request
 
+/**
+ * A [LocalRequest] consists in:
+ * <ul>
+ *     <li>Loading data from a specific file [path]</li>
+ *     <li>Deserializing data in a [ConcurrentHashMap] according to a [ConverterType]</li>
+ * </ul>
+ */
 internal data class LocalRequest(val context: Context, val path: String, val converterType: ConverterType) : Request
